6a01f9c9ff087d118848704db4758e64d90acd4d
pigji <wjlqlrll@naver.com>
2024-11-07 17:41:40 +0900

    11.07_2nd

--- 메르세데스_벤츠코리아/js/script.js
+++ 메르세데스_벤츠코리아/js/script.js
@@ -1,3 +1,4 @@
+//top버튼을 누르면 상단으로 이동하는 기능구현
 //top버튼 선택
 const btn = document.querySelector(".Topmove_button");
 
@@ -26,3 +27,16 @@ btn.addEventListener('click', (e) => {
     behavior: 'smooth'  //부드러운 이동
   })
 })
+
+/*--------------------------------------------------*/
+//슬라이드 배너
+
+//목록을 감싸고 있는 요소 선택
+const list = document.querySelector("#banner");
+//li요소 선택
+const listLi = document.querySelectorAll("#banner li");
+
+//한번에 보여줄 슬라이드 수
+const show_num = 2;
+//전체 슬라이드 갯수
+const total = listLi.length;
