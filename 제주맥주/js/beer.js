615606ad6a3b515776ec033dfc29e9a178a4ef8b
pigji <wjlqlrll@naver.com>
2025-01-06 18:04:01 +0900

    2025.01.06_2nd

--- 제주맥주/js/beer.js
+++ 제주맥주/js/beer.js
@@ -11,3 +11,32 @@ const swiper = new Swiper('.swiper-banner-box .swiper', {
   delay: 2000,
   }
 });
+
+
+//top버튼
+const btn = document.querySelector(".topBtn3");
+
+//브라우저 내에서 스크롤 이벤트가 발생했을때
+addEventListener('scroll', () => {
+  //변수 생성
+  let scrollTop = window.scrollY; //스크롤 이동 값
+  
+  if(scrollTop > 40){
+    btn.style.display = 'block';
+  }
+  else{
+    btn.style.display = 'none';
+  }
+});
+
+//버튼을 클릭하면 브라우저 최상단으로 이동
+btn.addEventListener('click', (e) => {
+  e.preventDefault(); //a태그 링크기능 제거
+
+  //window에 srollTo API를 사용하여 페이지 상단으로 부드럽게 스크롤 이동
+  window.scrollTo({
+    left: 0,
+    top: 0,
+    behavior: 'smooth'  //부드러운 스크롤 이동
+  })
+})
