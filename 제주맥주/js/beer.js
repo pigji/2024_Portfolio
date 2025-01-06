7fbbdeded2e2258907b9dc421ce9e1e3e88b9af3
pigji <wjlqlrll@naver.com>
2025-01-06 16:55:00 +0900

    2025.01.06_1nd

--- 제주맥주/js/beer.js
+++ 제주맥주/js/beer.js
@@ -1,7 +1,7 @@
 //swiper API
 const swiper = new Swiper('.swiper-banner-box .swiper', {
   loop: true,   //무한반복
-  slidesPerView: 4,    //화면에 보여줄 슬라이드 갯수 설정
+  slidesPerView: 5,    //화면에 보여줄 슬라이드 갯수 설정
   spaceBetween: 10,    //슬라이드 간격 설정
   slidesPerGroup: 1,   //슬라이드 될때 보여지는 갯수
   // speed: 1000,        //슬라이드 회전 속도
